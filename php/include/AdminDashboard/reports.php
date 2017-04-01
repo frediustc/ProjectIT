90792edfd989fbd8704ba4256a01fe68bb41ec21
frediustc <frediustc@gmail.com>
2017-04-01 18:27:41 +0000

    changed report for admin dasboard by removing checkboxes and delete buttons

---
+++ php/include/AdminDashboard/reports.php
@@ -0,0 +1,75 @@
+<!-- reports-->
+<div class="col-xs-8">
+    <h3>Lastest Reports</h3>
+    <table class="lastest-reports table">
+        <thead class="text-uppercase">
+            <tr>
+                <th>Customers</th>
+                <th>Reports</th>
+                <th>Reports Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="text-capitalize"><a href="#"><span class="img-sizing"><img src="media/images/pp/1.jpg" class="img-responsive img-circle"alt="pp cust"/></span>Fredius Tout...</a></td>
+                <td><a href="#">Fredius Tout Court wutewu wieuwe wiyw dwiyd wiydgwd iiwygd wdygwd wuhdgkj\ </a></td>
+                <td class="text-capitalize">21<sup>St</sup> Juin 2016</td>
+            </tr>
+            <tr>
+                <td class="text-capitalize"><a href="#"><span class="img-sizing"><img src="media/images/pp/1.jpg" class="img-responsive img-circle"alt="pp cust"/></span>Fredius Tout...</a></td>
+                <td><a href="#">Fredius Tout Court wutewu wieuwe wiyw dwiyd wiydgwd iiwygd wdygwd wuhdgkj\ syugji daiudgb</a></td>
+                <td class="text-capitalize">21<sup>St</sup> Juin 2016</td>
+            </tr>
+            <tr>
+                <td class="text-capitalize"><a href="#"><span class="img-sizing"><img src="media/images/pp/1.jpg" class="img-responsive img-circle"alt="pp cust"/></span>Fredius Tout...</a></td>
+                <td><a href="#">Fredius Tout Court wutewu wieuwe wiyw dwiyd wiydgwd iiwygd wdygwd wuhdgkj\ syugji daiudgb</a></td>
+                <td class="text-capitalize">21<sup>St</sup> Juin 2016</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<!--
+//Good version
+
+<div class="col-xs-8">
+    <h3>Lastest Reports</h3>
+    <table class="lastest-reports table">
+        <thead class="text-uppercase">
+            <tr>
+                <th><label for="check-all-reports"><span class="check-all-reports chk-lab"></span></label>
+                    <input type="checkbox" name="check-all-reports" id="check-all-reports" value="check-all-reports"/></th>
+                <th>Customers</th>
+                <th>Reports</th>
+                <th>Reports Date</th>
+                <th class="text-center"><button class="btn disabled btn-danger delete-all-reports">Delete</button></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><label for="check-report-1"><span class="chk-lab actived"></span></label>
+                    <input type="checkbox" name="check-report-1"  id="check-report-1" value="check-report-1"/></td>
+                <td class="text-capitalize"><a href="#"><span class="img-sizing"><img src="media/images/pp/1.jpg" class="img-responsive img-circle"alt="pp cust"/></span>Fredius Tout...</a></td>
+                <td><a href="#">Fredius Tout Court wutewu wieuwe wiyw dwiyd wiydgwd iiwygd wdygwd wuhdgkj\ </a></td>
+                <td class="text-capitalize">21<sup>St</sup> Juin 2016</td>
+                <td class="text-capitalize text-center"><button class="btn btn-danger">Delete</button></td>
+            </tr>
+            <tr>
+                <td><label for="check-report-1"><span class="chk-lab"></span></label>
+                    <input type="checkbox" name="check-report-1"  id="check-report-1" value="check-report-1"/></td>
+                <td class="text-capitalize"><a href="#"><span class="img-sizing"><img src="media/images/pp/1.jpg" class="img-responsive img-circle"alt="pp cust"/></span>Fredius Tout...</a></td>
+                <td><a href="#">Fredius Tout Court wutewu wieuwe wiyw dwiyd wiydgwd iiwygd wdygwd wuhdgkj\ syugji daiudgb</a></td>
+                <td class="text-capitalize">21<sup>St</sup> Juin 2016</td>
+                <td class="text-capitalize text-center"><button class="btn btn-danger">Delete</button></td>
+            </tr>
+            <tr>
+                <td><label for="check-report-1"><span class="chk-lab"></span></label>
+                    <input type="checkbox" name="check-report-1"  id="check-report-1" value="check-report-1"/></td>
+                <td class="text-capitalize"><a href="#"><span class="img-sizing"><img src="media/images/pp/1.jpg" class="img-responsive img-circle"alt="pp cust"/></span>Fredius Tout...</a></td>
+                <td><a href="#">Fredius Tout Court wutewu wieuwe wiyw dwiyd wiydgwd iiwygd wdygwd wuhdgkj\ syugji daiudgb</a></td>
+                <td class="text-capitalize">21<sup>St</sup> Juin 2016</td>
+                <td class="text-capitalize text-center"><button class="btn btn-danger">Delete</button></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+-->
