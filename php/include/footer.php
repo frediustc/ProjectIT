1a5fdf3efa09743df24fd7640c157cba4c70936d
frediustc <frediustc@gmail.com>
2017-04-01 14:20:34 +0000

    chart finished

--- php/include/footer.php
+++ php/include/footer.php
@@ -5,7 +5,8 @@
     <script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="js/bootstrap.me.js"></script>
     <!-- High Chart -->
-    <?php echo ($page === 'Admin Dashboard') ? '<script src="js/plugins/highcharts.js"></script><script src="js/Admin.Chart.js"></script>' : null; ?>
+    <?php echo ($page === 'Admin Dashboard') ? '<script src="js/plugins/highcharts.js"></script>' : null; ?>
+    <?php echo ($page === 'Admin Dashboard') ? '<script src="http://chartwidget/myChart.js"></script>' : null; ?>
     <!--general js script -->
     <script src="js/general.js"></script>
     <!-- for admin dashboard -->
@@ -17,7 +18,13 @@
         firstDay: 1,
         showOtherMonths: true,
         dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-    });" : null; ?>
+    });
+    var vis_list = [10, 20, 30, 40, 50, 60];
+    var rep_list = [60, 50, 40, 30, 20, 10];
+    var ord_list = [10, 50, 30, 60, 50, 20];
+    createChart('#visitors', vis_list, 'Uniques Visitors', '#4CB8FF');
+    createChart('#orders', ord_list, 'Orders', '#FE7167');
+    createChart('#reports', rep_list, 'Reports', '#00C484'); " : null; ?>
     </script>
 </body>
 
