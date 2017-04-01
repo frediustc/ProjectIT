15b1d646914c2d76948861016eeb2f0adaa27fc0
frediustc <frediustc@gmail.com>
2017-04-01 15:37:48 +0000

    editing list continous

--- Index.php
+++ Index.php
@@ -59,25 +59,44 @@ include("php/include/menu.php");
         </div>
     </div>
     <div class="col-sm-3 notif">
-        <div>
-            <h3 class="text-center notif-title">Notifications</h3>
-            <ul class="list-unstyled notif-list">
-                <li>
-                    <h5 class="text-capitalize"><b>Fredius Tout Court</b></h5>
-                    <p>Has been rented by Barklays</p>
-                    <p class="disabled">
-                        2 days ago<span class="notif-type ddfj-usericon"></span>
-                    </p>
-                </li>
-                <li>
-                    <h5 class="text-capitalize"><b>Fredius Tout Court</b></h5>
-                    <p>Has been rented by Barklays</p>
-                    <p class="disabled">
-                        2 days ago<span class="notif-type ddfj-usericon"></span>
-                    </p>
-                </li>
-            </ul>
-        </div>
+        <h3 class="text-center notif-title">Notifications</h3>
+        <ul class="list-continous notif-list">
+            <li class="messages">
+                <h5 class="text-capitalize"><b>Fredius Tout Court</b></h5>
+                <p>Has been rented by Barklays</p>
+                <p class="disabled">
+                    2 days ago<span class="notif-type ddfj-usericon"></span>
+                </p>
+            </li>
+            <li class="reports">
+                <h5 class="text-capitalize"><b>Fredius Tout Court</b></h5>
+                <p>Has been rented by Barklays</p>
+                <p class="disabled">
+                    2 days ago<span class="notif-type ddfj-usericon"></span>
+                </p>
+            </li>
+            <li class="orders">
+                <h5 class="text-capitalize"><b>Fredius Tout Court</b></h5>
+                <p>Has been rented by Barklays</p>
+                <p class="disabled">
+                    2 days ago<span class="notif-type ddfj-usericon"></span>
+                </p>
+            </li>
+            <li class="uploads">
+                <h5 class="text-capitalize"><b>Fredius Tout Court</b></h5>
+                <p>Has been rented by Barklays</p>
+                <p class="disabled">
+                    2 days ago<span class="notif-type ddfj-usericon"></span>
+                </p>
+            </li>
+            <li class="rents">
+                <h5 class="text-capitalize"><b>Fredius Tout Court</b></h5>
+                <p>Has been rented by Barklays</p>
+                <p class="disabled">
+                    2 days ago<span class="notif-type ddfj-usericon"></span>
+                </p>
+            </li>
+        </ul>
     </div>
 </div>
 
