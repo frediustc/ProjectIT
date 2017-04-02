77fce6a78acd23225e853aecfa89afc0b4dfd879
frediustc <frediustc@gmail.com>
2017-04-02 15:52:19 +0000

    starting reports page

--- Account/Index.php
+++ Account/Index.php
@@ -5,9 +5,10 @@ include($rep . "php/include/head.php");
 include($rep . "php/include/menu.php");
 ?>
 <div class="row">
-    <div class="col-sm-4">
+    <div class="col-sm-2">
         <div class="box box-left box-primary">
-            ok
+            <p><strong>Total Reports</strong></p>
+            <h1>1200</h1>
         </div>
     </div>
 </div>
