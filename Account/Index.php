3cf3d1ce6b6f671cf36d7d8db6dda51a7a4e64a3
frediustc <frediustc@gmail.com>
2017-04-02 15:33:15 +0000

    admin account page added

---
+++ Account/Index.php
@@ -0,0 +1,14 @@
+<?php
+$page = "Admin Account";
+$rep = "../";
+include($rep . "php/include/head.php");
+include($rep . "php/include/menu.php");
+?>
+<div class="row">
+    <div class="col-sm-4">
+        <div class="box box-left box-primary">
+            ok
+        </div>
+    </div>
+</div>
+<?php include($rep . "php/include/footer.php");?>
