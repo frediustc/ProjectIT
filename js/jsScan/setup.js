f56f4495e79a8b9eee46519f4a1a78f28ba43f40
frediustc <frediustc@gmail.com>
2017-04-07 19:00:47 +0000

    starting setup

--- js/jsScan/setup.js
+++ js/jsScan/setup.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function() {
-    
+    $('input').on('keyup', function(){
+        if($.trim($(this).val()) === '') {
+            $(this).parent().next().removeClass('hide')
+        }
+        else {
+            if(!$(this).parent().next().hasClass('hide'))
+            {
+                $(this).parent().next().addClass('hide');
+            }
+        }
+    });
 });
---
+++ php/script/setup.php
@@ -0,0 +1,6 @@
+<?php
+    // if(isset($_POST['setup-server']))
+if(isset($_POST['setup-server']) && !empty($_POST['setup-server']))
+{
+    
+}
