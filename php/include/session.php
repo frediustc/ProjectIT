e96c0733bbb381fbf8f9668db39b4373b897aaf9
frediustc <frediustc@gmail.com>
2017-04-09 13:44:43 +0000

    restructure of setup

--- php/include/session.php
+++ php/include/session.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
-if(isset($_SESSION['id']))
+if(isset($_SESSION['user_id']))
 {
+    if($page === 'Setup page' || $page === 'Administrator logIn')
+    {
+        header('location:' . $rep);
+    }
     require $rep . '/php/include/user.php';
+}
+else
+{
+    if($page !== 'Setup page' && $page !== 'Administrator logIn')
+    {
+        header('location:' . $rep);
+    }
 }
---
+++ setup/Index.php
@@ -0,0 +1,44 @@
+<?php
+$rep = "../";
+$page = "Setup page";
+include $rep .'php/include/head.php';
+?>
+    <div class="container">
+        <h1 class="text-center text-uppercase">Administrator Setup page</h1>
+        <div class="row">
+            <div class="col-xs-4 col-xs-push-4">
+                <div class="">
+                    <form class="adminForm form-vertical box box-top box-primary" method="post" action="./">
+                        <?php include('php/script/Setup.php'); ?>
+                        <div class="form-group">
+                            <label for="setup-server">Server Name</label>
+                            <input type="text" name="setup-server" required="" id="setup-server" class="form-control" />
+                        </div>
+                        <p class="alert bg-danger hide"><span class="glyphicon glyphicon-warning-sign"></span>Server Name is empty</p>
+                        <div class="form-group">
+                            <label for="setup-user">Database Username</label>
+                            <input type="text" name="setup-user" required="" id="setup-user" class="form-control" />
+                        </div>
+                        <p class="alert bg-danger hide"><span class="glyphicon glyphicon-warning-sign"></span>UserName is empty</p>
+                        <div class="form-group">
+                            <label for="setup-dbname">Database Name</label>
+                            <input type="text" name="setup-dbname" required="" id="setup-dbname" class="form-control" />
+                        </div>
+                        <p class="alert bg-danger hide"><span class="glyphicon glyphicon-warning-sign"></span>Database Name is empty</p>
+                        <div class="form-group">
+                            <label for="setup-pwd">Password</label>
+                            <input type="password" name="setup-pwd" id="setup-pwd" class="form-control" />
+                        </div>
+                        <p class="alert bg-primary hide"><span class="glyphicon glyphicon-warning-sign"></span>is your password really empty?</p>
+                        <div class="form-group text-center">
+                            <input type="submit" name="setup-page" value="Setup Page" class="btn-lg btn btn-primary btn-3d"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../js/jsScan/setup.js"></script>
+<?php include $rep .'php/include/footer.php';
+// header('location: Dashboard/');
+?>
