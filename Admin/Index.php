f4cc043cff3c4a582747192b940f3ff4ff6b88c3
frediustc <frediustc@gmail.com>
2017-04-09 12:18:56 +0000

    login init

--- Admin/Index.php
+++ Admin/Index.php
@@ -4,7 +4,7 @@ $rep = "./";
 $page = "Administrator logIn";
 include './php/include/head.php';
 ?>
-    <!-- <div class="container">
+    <div class="container">
         <h1 class="text-center text-uppercase">Administrator login page</h1>
         <div class="row">
             <div class="col-xs-4 col-xs-push-4">
@@ -14,10 +14,12 @@ include './php/include/head.php';
                             <label for="admin-email">Email address</label>
                             <input type="email" name="admin-email" required="" id="admin-email" class="form-control" />
                         </div>
+                        <p class="alert bg-danger hide"><span class="glyphicon glyphicon-warning-sign"></span>Server Name is empty</p>
                         <div class="form-group">
                             <label for="admin-pwd">Password</label>
                             <input type="password" name="admin-pwd" required="" id="admin-pwd" class="form-control" />
                         </div>
+                        <p class="alert bg-danger hide"><span class="glyphicon glyphicon-warning-sign"></span>Server Name is empty</p>
                         <div class="form-group text-center">
                             <input type="submit" name="admin-login" value="LogIn" class="btn-lg btn btn-primary btn-3d"/>
                         </div>
@@ -25,6 +27,4 @@ include './php/include/head.php';
                 </div>
             </div>
         </div>
-    </div> -->
-<?php include './php/include/footer.php';
-?>
+    </div>
