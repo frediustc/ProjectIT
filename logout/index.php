1760178c1e8f0c10c803e36ee74f4a60b2a441b7
Fredius Tout Court <frediustc@gmail.com>
2017-04-20 00:14:40 +0000

    log out  fuck

--- logout/index.php
+++ logout/index.php
@@ -1,4 +1,8 @@
 <?php
 session_start();
 session_destroy();
-header('location: ' . $_SERVER['HTTP_REFERER']);
+if(isset($_SERVER['HTTP_REFERER']))
+{
+    header('location: ' . $_SERVER['HTTP_REFERER']);
+} ?>
+<h1>FUCK U!!!</h1>
